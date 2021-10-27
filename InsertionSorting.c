d310b777b223f262aa53b756f2cc56b0788dc17b
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2021-10-27 22:27:31 +0530

    Update InsertionSorting.c

--- InsertionSorting.c
+++ InsertionSorting.c
@@ -2,38 +2,39 @@
 #include <conio.h>
 #include <limits.h>
 int InsSort(int ar[50], int size);
-void main()
+int main()
 {
     system("cls");
-    int ar[50], item, n, index,i;
-    printf("\nEnter desired array size: ");
+    int arr[50], item, n, index, i;
+    printf("\nEnter desired array size (upto 50): ");
     scanf("%d", &n);
-    printf("\nEnter array elements\n");
+    printf("\nEnter array elements:\n");
     for (i = 1; i <= n; i++)
     {
-        scanf("%d", &ar[i]);
+        scanf("%d", &arr[i]);
     }
-    InsSort(ar, n);
-    printf("\nSorted array is:\n");
+    InsSort(arr, n);
+    printf("\nSorted array:\n");
     for (i = 1; i <= n; i++)
     {
-        printf("%d\t", ar[i]);
+        printf("%d\t", arr[i]);
     }
     getch();
+    return 0;
 }
-int InsSort(int ar[50], int size)
+int InsSort(int arr[50], int size)
 {
     int temp, j;
-    ar[0] = INT_MIN;
+    arr[0] = INT_MIN;
     for (int i = 1; i <= size; i++)
     {
-        temp = ar[i];
+        temp = arr[i];
         j = i - 1;
-        while (temp < ar[j])
+        while (temp < arr[j])
         {
-            ar[j + 1] = ar[j];
+            arr[j + 1] = arr[j];
             j--;
         }
-        ar[j + 1] = temp;
+        arr[j + 1] = temp;
     }
 }
